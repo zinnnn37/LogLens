a8cefb4aff7748ca97fd07f46b9852c413e4e810
김민진 <zinnnn37@gmail.com>
2025-10-21 16:42:36 +0900

    chore: Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+// vite.config.ts
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'soo1Loglens',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      output: {
+        // 이렇게 하면 require() 사용 시 default 객체 없이 바로 접근 가능
+        exports: 'named',
+        globals: {
+          soo1Loglens: 'soo1Loglens', // 글로벌 네임스페이스 설정
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      // 타입 정의 파일도 'dist' 폴더에 생성되도록 명시
+      outDir: 'dist',
+      insertTypesEntry: true,
+    }),
+  ],
+});
