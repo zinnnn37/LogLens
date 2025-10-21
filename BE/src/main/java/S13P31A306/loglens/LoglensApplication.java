648cbdad72099d943c28c05ada63e290fa9c08ec
Ukjong <xb253q@gmail.com>
2025-10-21 11:36:03 +0900

    Initial commit

---
+++ BE/src/main/java/S13P31A306/loglens/LoglensApplication.java
@@ -0,0 +1,13 @@
+package S13P31A306.loglens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoglensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoglensApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/S13P31A306/loglens/LoglensApplicationTests.java
@@ -0,0 +1,13 @@
+package S13P31A306.loglens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoglensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
