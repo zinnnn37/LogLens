fa4d82137086d238efd602f610b3da5eac31745d
김민진 <zinnnn37@gmail.com>
2025-10-22 15:57:22 +0900

    feat: 로깅 데코레이터, 포매터, 트레이스 래퍼, 로그 수집기 추가

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Log } from './decorators/logs';
 export { LogFormatter } from './utils/logFormatter';
+export { withLogLens } from './wrappers/trace';
+export { LogCollector } from './core/logCollector';
