9cf9e6cf013a9673bba891dfc02f861771184352
Ukjong <xb253q@gmail.com>
2025-10-22 11:23:08 +0900

    feat(global): ApiResponseFactory 클래스 추가
    • 성공/실패 응답 생성을 위한 팩토리 클래스 추가
    • SuccessCode 및 ErrorCode 기반 응답 생성
    • ResponseEntity를 사용하여 HTTP 응답 반환

---
+++ src/main/java/S13P31A306/loglens/global/dto/response/ApiResponseFactory.java
@@ -0,0 +1,23 @@
+package S13P31A306.loglens.global.dto.response;
+
+import S13P31A306.loglens.global.constants.ErrorCode;
+import S13P31A306.loglens.global.constants.SuccessCode;
+import org.springframework.http.ResponseEntity;
+
+public class ApiResponseFactory {
+    public static <T> ResponseEntity<SuccessResponse<T>> success(final SuccessCode successCode, final T data) {
+        return ResponseEntity.status(successCode.getStatus()).body(SuccessResponse.of(successCode, data));
+    }
+
+    public static ResponseEntity<BaseResponse> success(final SuccessCode successCode) {
+        return ResponseEntity.status(successCode.getStatus()).body(SuccessResponse.of(successCode));
+    }
+
+    public static <T> ResponseEntity<ErrorResponse<T>> fail(final ErrorCode errorCode, final T data) {
+        return ResponseEntity.status(errorCode.getStatus()).body(ErrorResponse.of(errorCode, data));
+    }
+
+    public static ResponseEntity<BaseResponse> fail(final ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getStatus()).body(ErrorResponse.of(errorCode));
+    }
+}
