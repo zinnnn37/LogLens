742e9bb981dbe5623b498c25d21a24bca9c24b32
Ukjong <xb253q@gmail.com>
2025-10-22 14:54:33 +0900

    refactor(core): BaseEntity 클래스 업데이트
    - 불필요한 줄바꿈 제거
    - 주석 추가 및 수정
    - 사용하지 않는 import 제거

--- src/main/java/S13P31A306/loglens/global/entity/BaseEntity.java
+++ src/main/java/S13P31A306/loglens/global/entity/BaseEntity.java
@@ -1,20 +1,17 @@
 package S13P31A306.loglens.global.entity;
 
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 
 // @formatter:off
 /**
- * 모든 엔티티에서 공통적으로 사용하는 속성(createdAt, updatedAt, deleted 등)을 정의한 추상 클래스
+ * 모든 엔티티에서 공통적으로 사용하는 속성(createdAt, updatedAt 등)을 정의한 추상 클래스
  * - @MappedSuperclass: 해당 클래스를 상속한 자식 엔티티가 이 필드들을 컬럼으로 인식하게 합니다.
  * - @EntityListeners(AuditingEntityListener.class): Date 필드를 자동으로 관리하기 위해 등록합니다.
  */
@@ -31,4 +28,6 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    // delete 컬럼 고민
 }
