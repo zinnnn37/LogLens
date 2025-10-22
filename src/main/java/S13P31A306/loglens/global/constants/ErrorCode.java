1a8a6921a16e1d89138080b52c21f44ec4f5764d
Ukjong <xb253q@gmail.com>
2025-10-22 13:12:50 +0900

    feat(global): ErrorCode에 safeValueOf 메서드 추가
    • ErrorCode enum을 안전하게 조회하는 기능 추가
    • IllegalArgumentException 처리
    • GlobalErrorCode에서 enum name으로 조회

--- src/main/java/S13P31A306/loglens/global/constants/ErrorCode.java
+++ src/main/java/S13P31A306/loglens/global/constants/ErrorCode.java
@@ -2,6 +2,8 @@ package S13P31A306.loglens.global.constants;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.Optional;
+
 public interface ErrorCode {
     String getCode();
     String getMessage();
@@ -10,6 +12,19 @@ public interface ErrorCode {
     // HTTP Status와 연동
     default HttpStatus getHttpStatus() {
         return HttpStatus.valueOf(getStatus());
+    }
+
+    /**
+     * Enum name으로 ErrorCode를 안전하게 조회한다.
+     * @param name ErrorCode enum의 이름
+     * @return 존재하면 해당 ErrorCode, 없으면 Optional.empty()
+     */
+    static Optional<ErrorCode> safeValueOf(String name) {
+        try {
+            return Optional.of(GlobalErrorCode.valueOf(name));
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
     }
 }
 
