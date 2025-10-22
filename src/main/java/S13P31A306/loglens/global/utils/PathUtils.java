0599d930ce88dbb8b61747fdab8e83082e33df04
Ukjong <xb253q@gmail.com>
2025-10-22 13:13:09 +0900

    feat(global): 유틸리티 클래스 PathUtils 추가
    • 경로에서 마지막 속성 추출 기능 추가
    • 유틸리티 클래스 오류 메시지 상수 사용
    • 단일 파일 변경 및 간단한 복잡도

---
+++ src/main/java/S13P31A306/loglens/global/utils/PathUtils.java
@@ -0,0 +1,20 @@
+package S13P31A306.loglens.global.utils;
+
+import S13P31A306.loglens.global.constants.Messages;
+
+public final class PathUtils {
+    private PathUtils() {
+        throw new IllegalStateException(Messages.UTILITY_CLASS_ERROR.message());
+    }
+
+    /**
+     * 경로 문자열에서 마지막 필드명만 추출
+     *
+     * @param path 점(.)으로 구분된 경로 문자열 (예: "method.param.field")
+     * @return 마지막 필드명 (예: "field")
+     */
+    public static String extractLeafProperty(final String path) {
+        final var lastDot = path.lastIndexOf('.');
+        return (lastDot != -1) ? path.substring(lastDot + 1) : path;
+    }
+}
