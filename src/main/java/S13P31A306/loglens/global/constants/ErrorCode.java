ef1bea868beca8b29fac3379e092e1d650962f32
Ukjong <xb253q@gmail.com>
2025-10-22 13:36:32 +0900

    refactor(core): ErrorCode 인터페이스 정리
    - ErrorCode 인터페이스의 불필요한 import 제거
    - HTTP 상태 연동 방식 유지
    - 안전한 enum 조회 메서드 제거

--- src/main/java/S13P31A306/loglens/global/constants/ErrorCode.java
+++ src/main/java/S13P31A306/loglens/global/constants/ErrorCode.java
@@ -2,8 +2,6 @@ package S13P31A306.loglens.global.constants;
 
 import org.springframework.http.HttpStatus;
 
-import java.util.Optional;
-
 public interface ErrorCode {
     String getCode();
     String getMessage();
@@ -12,19 +10,6 @@ public interface ErrorCode {
     // HTTP Status와 연동
     default HttpStatus getHttpStatus() {
         return HttpStatus.valueOf(getStatus());
-    }
-
-    /**
-     * Enum name으로 ErrorCode를 안전하게 조회한다.
-     * @param name ErrorCode enum의 이름
-     * @return 존재하면 해당 ErrorCode, 없으면 Optional.empty()
-     */
-    static Optional<ErrorCode> safeValueOf(String name) {
-        try {
-            return Optional.of(GlobalErrorCode.valueOf(name));
-        } catch (IllegalArgumentException e) {
-            return Optional.empty();
-        }
     }
 }
 
