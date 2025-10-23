8b2e9d085a78990993fd01472304cbca9888adad
LeeJongHyun <a24358232@gmail.com>
2025-10-23 11:02:24 +0900

    feat: 라우터 구조 변경, Layout 적용 페이지 그룹화 및 미적용 페이지 분리

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,33 +1,35 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
 
-import Layout from '@/components/layout/Layout';
-import Home from '@/pages/Home';
-import NotFound from '@/pages/NotFound';
-import { ROUTE_PATH } from '@/router/route-path';
-import LoginPage from '@/pages/LoginPage';
-import SignupPage from '@/pages/SignupPage';
+import Layout from "@/components/layout/Layout";
+import Home from "@/pages/Home";
+import NotFound from "@/pages/NotFound";
+import { ROUTE_PATH } from "@/router/route-path";
+import LoginPage from "@/pages/LoginPage";
+import SignupPage from "@/pages/SignupPage";
 
 export const router = createBrowserRouter([
+  // Layout 적용할 페이지 그룹
   {
-    path: '/',
     element: <Layout />,
     children: [
       {
-        index: true,
+        path: "/",
         element: <Home />,
       },
       {
         path: ROUTE_PATH.NOT_FOUND,
         element: <NotFound />,
       },
-      {
-        path: ROUTE_PATH.LOGIN,
-        element: <LoginPage />,
-      },
-      {
-        path: ROUTE_PATH.SIGNUP,
-        element: <SignupPage />,
-      },
     ],
+  },
+
+  // Layout 미적용 그룹
+  {
+    path: ROUTE_PATH.LOGIN,
+    element: <LoginPage />,
+  },
+  {
+    path: ROUTE_PATH.SIGNUP,
+    element: <SignupPage />,
   },
 ]);
