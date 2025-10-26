20c95d900cd576e71620701e90151dc117708a66
김건학 <kkh10002@naver.com>
2025-10-26 14:37:45 +0900

    fix: 이름으로 유저 검색 시 List 반환하도록 수정

--- src/main/java/S13P31A306/loglens/domain/auth/respository/UserRepository.java
+++ src/main/java/S13P31A306/loglens/domain/auth/respository/UserRepository.java
@@ -1,13 +1,14 @@
 package S13P31A306.loglens.domain.auth.respository;
 
 import S13P31A306.loglens.domain.auth.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByName(String name);
+    List<User> findByName(String name);
 
     boolean existsByEmail(String email);
 }
