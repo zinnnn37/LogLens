ce82a08ec68b4cce41da52a80c908b3de09fc3ce
김건학 <kkh10002@naver.com>
2025-10-26 23:23:16 +0900

    feat: JPA auditing 활성화

--- src/main/java/S13P31A306/loglens/LoglensApplication.java
+++ src/main/java/S13P31A306/loglens/LoglensApplication.java
@@ -2,7 +2,9 @@ package S13P31A306.loglens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LoglensApplication {
 
