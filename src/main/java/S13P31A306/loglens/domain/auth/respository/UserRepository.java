303db8520903f173e852b6e9a5c693b1ea0dce91
김건학 <kkh10002@naver.com>
2025-10-26 16:34:07 +0900

    fix: 사용자 id 타입을 Long에서 Integer로 수정

--- src/main/java/S13P31A306/loglens/domain/auth/respository/UserRepository.java
+++ src/main/java/S13P31A306/loglens/domain/auth/respository/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
     List<User> findByName(String name);
