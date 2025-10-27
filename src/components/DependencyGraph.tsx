3d6423a27a6f2be15aca2116225e43ee53c82cab
LeeJongHyun <a24358232@gmail.com>
2025-10-27 14:13:31 +0900

    feat: 의존성 그래프 페이지 용 컴포넌트 파일 생성

---
+++ src/components/DependencyGraph.tsx
@@ -0,0 +1,10 @@
+const DependencyGraph = () => {
+    return (
+        <div className="rounded-lg border bg-white p-6 shadow-sm">
+            결정 후 진행
+
+        </div>
+    )
+}
+
+export default DependencyGraph
--- src/pages/DependencyGraphPage.tsx
+++ src/pages/DependencyGraphPage.tsx
@@ -1,5 +1,13 @@
+import DependencyGraph from "@/components/DependencyGraph";
+
 const DependencyGraphPage = () => {
-  return <div>Dependency Graph Page</div>;
+  return (
+    <div className="font-pretendard space-y-6 p-6 py-1">
+      <h1 className="font-godoM text-lg">의존성 그래프</h1>
+
+      <DependencyGraph></DependencyGraph>
+    </div>);
+
 };
 
 export default DependencyGraphPage;
