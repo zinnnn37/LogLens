1c3fdb5f2701929e5f9401f5e2a21cc68c053abf
김민진 <zinnnn37@gmail.com>
2025-10-29 17:14:08 +0900

    fix: int -> Integer

--- src/main/java/S13P31A306/loglens/domain/project/dto/request/ProjectMemberInviteRequest.java
+++ src/main/java/S13P31A306/loglens/domain/project/dto/request/ProjectMemberInviteRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.NotNull;
 public record ProjectMemberInviteRequest(
         @NotNull(message = "USER_ID_REQUIRED")
         @Schema(description = "초대할 사용자 ID", example = "42")
-        int userId
+        Integer userId
 ) {
 }
