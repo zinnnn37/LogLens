0dc82164a21e2d91d22bbcbafd4f68f0e45bd7f4
김건학 <kkh10002@naver.com>
2025-10-29 09:49:19 +0900

    fix: 이메일 유효성 검사 정규식 수정
    - 숫자로 허용하도록 수정

--- src/main/java/S13P31A306/loglens/domain/auth/dto/request/UserSignupRequest.java
+++ src/main/java/S13P31A306/loglens/domain/auth/dto/request/UserSignupRequest.java
@@ -17,7 +17,7 @@ public record UserSignupRequest(
 
         @NotBlank(message = "EMAIL_REQUIRED")
         @Email(message = "EMAIL_INVALID_FORMAT")
-        @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z][a-zA-Z0-9.-]*\\.[a-zA-Z]{2,}$",
+        @Pattern(regexp = "^[a-zA-Z0-9][a-zA-Z0-9._-]*@[a-zA-Z][a-zA-Z0-9.-]*\\.[a-zA-Z]{2,}$\n",
                 message = "EMAIL_INVALID_FORMAT")
         @Schema(description = "사용자 이메일", example = "developer@example.com")
         String email,
