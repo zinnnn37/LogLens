a93b847ad820423dd69c355078537a5507e90135
김건학 <kkh10002@naver.com>
2025-10-29 19:21:16 +0900

    refactor: 응답 필드 PageResponse로 정리

--- src/main/java/S13P31A306/loglens/domain/auth/controller/impl/UserController.java
+++ src/main/java/S13P31A306/loglens/domain/auth/controller/impl/UserController.java
@@ -10,6 +10,7 @@ import S13P31A306.loglens.domain.auth.dto.response.UserSignupResponse;
 import S13P31A306.loglens.domain.auth.service.UserService;
 import S13P31A306.loglens.global.dto.response.ApiResponseFactory;
 import S13P31A306.loglens.global.dto.response.BaseResponse;
+import S13P31A306.loglens.global.dto.response.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -57,12 +58,18 @@ public class UserController implements UserApi {
 
     @Override
     @GetMapping("/users/search")
-    public ResponseEntity<? extends BaseResponse> findUsersByName(String name, Integer page, Integer size, String sort, String order) {
-        UserSortField sortField = UserSortField.valueOf(sort.toUpperCase());
-        Sort.Direction direction = Sort.Direction.fromString(order.toUpperCase());
-        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField.getFieldName()));
+    public ResponseEntity<? extends BaseResponse> findUsersByName(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "size", defaultValue = "20") Integer size,
+            @RequestParam(name = "sort", defaultValue = "CREATED_AT") String sort,
+            @RequestParam(name = "order", defaultValue = "DESC") String order) {
 
-        Page<UserSearchResponse> response = userService.findUsersByName(name, pageable);
-        return ApiResponseFactory.success(UserSuccessCode.USER_SEARCH_SUCCESS, response);
+        String sortField = UserSortField.valueOf(sort.toUpperCase()).getFieldName();
+        Sort.Direction direction = Sort.Direction.fromString(order.toUpperCase());
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+
+        Page<UserSearchResponse> userPage = userService.findUsersByName(name, pageable);
+        return ApiResponseFactory.success(UserSuccessCode.USER_SEARCH_SUCCESS, new PageResponse<>(userPage));
     }
 }
