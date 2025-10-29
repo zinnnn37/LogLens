2b4bc14e5f3d944c0a70def64cd16e72605484c0
김민진 <zinnnn37@gmail.com>
2025-10-29 16:37:12 +0900

    refactor: 프로젝트 성공 코드 이름 변경

--- src/main/java/S13P31A306/loglens/domain/project/constants/ProjectSuccessCode.java
+++ src/main/java/S13P31A306/loglens/domain/project/constants/ProjectSuccessCode.java
@@ -9,15 +9,14 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ProjectSuccessCode implements SuccessCode {
     // 200 OK
-    PROJECT_LIST_FETCH_SUCCESS("PJ200-1", "프로젝트 목록을 성공적으로 조회했습니다.", HttpStatus.OK.value()),
-    PROJECT_DETAIL_FETCH_SUCCESS("PJ200-2", "프로젝트 상세 정보를 성공적으로 조회했습니다.", HttpStatus.OK.value()),
-    PROJECT_DELETION_SUCCESS("PJ200-3", "프로젝트를 성공적으로 삭제했습니다.", HttpStatus.OK.value()),
-    PROJECT_MEMBER_DELETION_SUCCESS("PJ200-4", "멤버를 성공적으로 삭제했습니다.", HttpStatus.OK.value()),
+    PROJECT_LIST_RETRIEVED("PJ200-1", "프로젝트 목록을 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    PROJECT_RETRIEVED("PJ200-2", "프로젝트 상세 정보를 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    PROJECT_DELETED("PJ200-3", "프로젝트를 성공적으로 삭제했습니다.", HttpStatus.OK.value()),
+    MEMBER_DELETED("PJ200-4", "멤버를 성공적으로 삭제했습니다.", HttpStatus.OK.value()),
 
     // 201 CREATED
-    PROJECT_CREATION_SUCCESS("PJ201-1", "프로젝트가 성공적으로 생성되었습니다.", HttpStatus.CREATED.value()),
-    INVITATION_SUCCESS("PJ201-2", "멤버가 성공적으로 초대되었습니다.", HttpStatus.CREATED.value());
-
+    PROJECT_CREATED("PJ201-1", "프로젝트가 성공적으로 생성되었습니다.", HttpStatus.CREATED.value()),
+    MEMBER_INVITED("PJ201-2", "멤버가 성공적으로 초대되었습니다.", HttpStatus.CREATED.value());
     private final String code;
     private final String message;
     private final int status;
