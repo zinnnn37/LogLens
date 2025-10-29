e71b5188ffb2a098bed0d2f4a91505159e98c675
Ukjong <xb253q@gmail.com>
2025-10-29 16:50:34 +0900

    fix: 컴포넌트 타입 FE, BE, INFRA로 변경
    - 기존 "CONTROLLER", "SERVICE", "REPOSITORY" 값을 대체.
    - 컴포넌트 타입 의미 명확화.

--- src/main/java/a306/dependency_logger_starter/dependency/dto/ComponentRequest.java
+++ src/main/java/a306/dependency_logger_starter/dependency/dto/ComponentRequest.java
@@ -3,7 +3,7 @@ package a306.dependency_logger_starter.dependency.dto;
 public record ComponentRequest(
         String name,
         String classType,
-        String componentType,  // "CONTROLLER", "SERVICE", "REPOSITORY"
+        String componentType,  // "FE", "BE", "INFRA"
         String packageName,
         String layer,          // "CONTROLLER", "SERVICE", "REPOSITORY"
         String technology      // "Spring", "Java" 등
