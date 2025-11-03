c2a7ab5e4044d8ca6603bfb7d17690ba72a1c4fa
김민진 <zinnnn37@gmail.com>
2025-11-03 15:53:13 +0900

    refactor: DashboardController 이동 및 API 변경

--- src/main/java/S13P31A306/loglens/domain/dashboard/controller/impl/DashboardController.java
+++ src/main/java/S13P31A306/loglens/domain/dashboard/controller/impl/DashboardController.java
@@ -1,5 +1,7 @@
-package S13P31A306.loglens.domain.dashboard.controller;
+package S13P31A306.loglens.domain.dashboard.controller.impl;
 
+import S13P31A306.loglens.domain.dashboard.controller.DashboardApi;
+import S13P31A306.loglens.domain.dashboard.dto.response.DashboardOverviewResponse;
 import S13P31A306.loglens.domain.dashboard.service.DashboardService;
 import S13P31A306.loglens.global.dto.response.ApiResponseFactory;
 import S13P31A306.loglens.global.dto.response.BaseResponse;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-public class DashboardController {
+public class DashboardControllerImpl implements DashboardApi {
 
     private final DashboardService dashboardService;
 
@@ -23,7 +25,7 @@ public class DashboardController {
             @RequestParam(required = false) String startTime,
             @RequestParam(required = false) String endTime
     ) {
-        DashboardOverviewResponse response = dashboardService.getOverview(projectId);
+        DashboardOverviewResponse response = dashboardService.getStatisticsOverview(projectId, startTime, endTime);
         return ApiResponseFactory.success(
 
         );
