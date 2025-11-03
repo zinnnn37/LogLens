adcc865792dbfe7e93a5fc1eb244ae1ec3dc9c93
Ukjong <xb253q@gmail.com>
2025-11-03 15:30:43 +0900

    fix: 프로젝트 UUID로 프로젝트 조회 기능 추가
    - `findByProjectUuid` 메서드 추가
    - `Optional` 반환으로 존재하지 않을 경우 처리

--- src/main/java/S13P31A306/loglens/domain/project/repository/ProjectRepository.java
+++ src/main/java/S13P31A306/loglens/domain/project/repository/ProjectRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
 
@@ -33,4 +34,5 @@ public interface ProjectRepository extends JpaRepository<Project, Integer> {
             "WHERE p.id IN (SELECT pm.project.id FROM ProjectMember pm WHERE pm.user.id = :userId)")
     List<Project> findProjectsWithMembersByUserId(@Param("userId") int userId);
 
+    Optional<Project> findByProjectUuid(String apiKey);
 }
