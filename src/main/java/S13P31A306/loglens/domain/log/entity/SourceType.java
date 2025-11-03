f67d89a563f8d9f0ea94c37ffc61fefbe7e0da1e
김건학 <kkh10002@naver.com>
2025-11-03 15:27:06 +0900

    feat: 로그 레벨 및 소스 타입 enum 추가

--- src/main/java/S13P31A306/loglens/domain/log/entity/SourceType.java
+++ src/main/java/S13P31A306/loglens/domain/log/entity/SourceType.java
@@ -5,10 +5,10 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum LogLevel {
-    INFO("정보 로그"),
-    WARN("경고 로그"),
-    ERROR("오류 로그");
+public enum SourceType {
+    FE("프론트엔드 로그"),
+    BE("백엔드 로그"),
+    INFRA("인프라 로그");
 
     private final String description;
 }
