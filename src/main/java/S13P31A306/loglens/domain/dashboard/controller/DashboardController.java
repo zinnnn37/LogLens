f0b77bbc3f6313cb5b1e9da7c426360bff41b794
김민진 <zinnnn37@gmail.com>
2025-11-03 15:10:07 +0900

    feat: 대시보드 개요 API 추가

--- src/main/java/S13P31A306/loglens/domain/dashboard/controller/DashboardController.java
+++ src/main/java/S13P31A306/loglens/domain/dashboard/controller/DashboardController.java
@@ -1,4 +1,32 @@
 package S13P31A306.loglens.domain.dashboard.controller;
 
+import S13P31A306.loglens.domain.dashboard.service.DashboardService;
+import S13P31A306.loglens.global.dto.response.ApiResponseFactory;
+import S13P31A306.loglens.global.dto.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping("/statistics/overview")
+    public ResponseEntity<? extends BaseResponse> getOverview(
+            @RequestParam Integer projectId,
+            @RequestParam(required = false) String startTime,
+            @RequestParam(required = false) String endTime
+    ) {
+        DashboardOverviewResponse response = dashboardService.getOverview(projectId);
+        return ApiResponseFactory.success(
+
+        );
+    }
+
 }
