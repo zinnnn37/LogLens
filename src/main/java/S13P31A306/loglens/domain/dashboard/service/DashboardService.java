679a858e8ed51b3a6a4c647b9bd7468c135d16b8
김민진 <zinnnn37@gmail.com>
2025-11-03 15:53:27 +0900

    feat: 통계 개요 조회 API 추가

--- src/main/java/S13P31A306/loglens/domain/dashboard/service/DashboardService.java
+++ src/main/java/S13P31A306/loglens/domain/dashboard/service/DashboardService.java
@@ -1,4 +1,16 @@
 package S13P31A306.loglens.domain.dashboard.service;
 
+import S13P31A306.loglens.domain.dashboard.dto.response.DashboardOverviewResponse;
+
 public interface DashboardService {
+
+    /**
+     * 프로젝트 로그 통계 조회
+     *
+     * @param projectId 프로젝트 ID
+     * @param startTime 출력 데이터 필터 시작 시간
+     * @param endTime 출력 데이터 필터 종료 시간
+     */
+    DashboardOverviewResponse getStatisticsOverview(int projectId, String startTime, String endTime);
+
 }
