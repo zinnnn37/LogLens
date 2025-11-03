3f7d36a9b9a433d68b57f8a162877898e70ec27c
김민진 <zinnnn37@gmail.com>
2025-11-03 15:53:35 +0900

    feat: DashboardServiceImpl 구현

---
+++ src/main/java/S13P31A306/loglens/domain/dashboard/service/impl/DashboardServiceImpl.java
@@ -0,0 +1,8 @@
+package S13P31A306.loglens.domain.dashboard.service.impl;
+
+import S13P31A306.loglens.domain.dashboard.service.DashboardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardServiceImpl implements DashboardService {
+}
