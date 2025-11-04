4502077be018b6e4d25ca9d5148d62b8a34cd53c
김민진 <zinnnn37@gmail.com>
2025-11-04 15:20:24 +0900

    fix: 프로젝트 목록 조회 시 페이지 검증 추가

--- src/main/java/S13P31A306/loglens/domain/project/controller/impl/ProjectController.java
+++ src/main/java/S13P31A306/loglens/domain/project/controller/impl/ProjectController.java
@@ -1,16 +1,22 @@
 package S13P31A306.loglens.domain.project.controller.impl;
 
+import S13P31A306.loglens.domain.project.constants.ProjectPageNumber;
 import S13P31A306.loglens.domain.project.constants.ProjectSuccessCode;
 import S13P31A306.loglens.domain.project.controller.ProjectApi;
 import S13P31A306.loglens.domain.project.dto.request.ProjectCreateRequest;
+import S13P31A306.loglens.domain.project.dto.request.ProjectListRequest;
 import S13P31A306.loglens.domain.project.dto.request.ProjectMemberInviteRequest;
 import S13P31A306.loglens.domain.project.dto.response.*;
 import S13P31A306.loglens.domain.project.service.ProjectService;
 import S13P31A306.loglens.global.dto.response.ApiResponseFactory;
 import S13P31A306.loglens.global.dto.response.BaseResponse;
+import S13P31A306.loglens.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static S13P31A306.loglens.domain.project.constants.ProjectErrorCode.INVALID_PAGE_NUMBER;
+import static S13P31A306.loglens.domain.project.constants.ProjectErrorCode.INVALID_PAGE_SIZE;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -32,12 +38,17 @@ public class ProjectController implements ProjectApi {
 	@GetMapping
 	@Override
 	public ResponseEntity<? extends BaseResponse> getProjects(
-			int page,
-			int size,
-			String sort,
-			String order
+			@ModelAttribute ProjectListRequest request
 	) {
-		ProjectListResponse response = projectService.getProjects(page, size, sort, order);
+		// 검증
+		validatePageRequest(request);
+
+		ProjectListResponse response = projectService.getProjects(
+				request.page(),
+				request.size(),
+				request.getSortParam(),
+				request.getOrderParam()
+		);
 		return ApiResponseFactory.success(
 				ProjectSuccessCode.PROJECT_LIST_RETRIEVED,
 				response
@@ -89,4 +100,14 @@ public class ProjectController implements ProjectApi {
 				projectId
 		);
 	}
+
+	private void validatePageRequest(ProjectListRequest request) {
+		if (request.page() < ProjectPageNumber.MIN_PAGE_NUMBER) {
+			throw new BusinessException(INVALID_PAGE_NUMBER);
+		}
+		if (request.size() < ProjectPageNumber.MIN_PAGE_SIZE || request.size() > ProjectPageNumber.MAX_PAGE_SIZE) {
+			throw new BusinessException(INVALID_PAGE_SIZE);
+		}
+	}
+
 }
