781d83768c0aa914ada562bf9d6078446389b48e
LeeJongHyun <a24358232@gmail.com>
2025-11-04 10:43:50 +0900

    feat : 이름으로 멤버검색 API 함수 이중 파라미터 수정

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -11,15 +11,8 @@ import type {
 export const searchUsers = async (
   params: SearchUserParams,
 ): Promise<PaginatedUserSearchResponse> => {
-  try {
-    // GET의 두 번째 인자는 config 객체 → params는 반드시 이렇게 감싸야 함
-    const res = await apiClient.get<PaginatedUserSearchResponse>(
-      API_PATH.SEARCH_USERS,
-      { params },
-    );
-    return res;
-  } catch (error) {
-    console.error('멤버 검색 실패', error);
-    throw error;
-  }
+  return apiClient.get<PaginatedUserSearchResponse>(
+    API_PATH.SEARCH_USERS,
+    params,
+  );
 };
