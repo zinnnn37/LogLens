7520a1f4ca0406db3cbe464682a7d6646cc8fe5a
김민진 <zinnnn37@gmail.com>
2025-11-05 14:43:02 +0900

    fix: 에러/성공 코드 상수 추가 및 수정

--- src/main/java/S13P31A306/loglens/domain/dashboard/constants/DashboardSuccessCode.java
+++ src/main/java/S13P31A306/loglens/domain/dashboard/constants/DashboardSuccessCode.java
@@ -10,7 +10,12 @@ import org.springframework.http.HttpStatus;
 public enum DashboardSuccessCode implements SuccessCode {
 
     // 200 OK
-    OVERVIEW_RETRIVED("DSB200-1", "대시보드 통계를 성공적으로 조회했습니다.", HttpStatus.OK.value());
+    OVERVIEW_RETRIEVED("DSB200-1", "대시보드 통계를 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    FREQUENT_ERROR_RETRIEVED("DSB200-2", "자주 발생하는 에러를 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    API_STATISTICS_RETRIEVED("DSB200-3", "API 통계를 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    REQUEST_FLOW_RETRIEVED("DSB200-4", "요청 흐름을 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    HEATMAP_RETRIEVED("DSB200-5", "히트맵을 성공적으로 조회했습니다.", HttpStatus.OK.value()),
+    NOTIFICATION_FEED_RETRIEVED("DS200-6", "알림 피드를 성공적으로 조회했습니다.", HttpStatus.OK.value());
 
     private final String code;
     private final String message;
