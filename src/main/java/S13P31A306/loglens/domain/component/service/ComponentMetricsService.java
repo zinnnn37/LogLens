529a5dae12c188af54021624d34687f400cda1bb
Ukjong <xb253q@gmail.com>
2025-11-05 11:24:27 +0900

    feat: ComponentMetricsService 인터페이스 추가
    • 컴포넌트 메트릭 조회를 위한 인터페이스 정의
    • 단일/다중 컴포넌트 메트릭 조회 메서드 추가

---
+++ src/main/java/S13P31A306/loglens/domain/component/service/ComponentMetricsService.java
@@ -0,0 +1,25 @@
+package S13P31A306.loglens.domain.component.service;
+
+import S13P31A306.loglens.domain.component.entity.ComponentMetrics;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ComponentMetricsService {
+
+    /**
+     * 여러 컴포넌트의 최신 메트릭 조회 (캐시 사용)
+     *
+     * @param componentIds 컴포넌트 ID 리스트
+     * @return componentId -> ComponentMetrics 맵
+     */
+    Map<Integer, ComponentMetrics> getMetricsByComponentIds(List<Integer> componentIds);
+
+    /**
+     * 단일 컴포넌트의 최신 메트릭 조회 (캐시 사용)
+     *
+     * @param componentId 컴포넌트 ID
+     * @return 메트릭 정보 (없으면 기본값)
+     */
+    ComponentMetrics getMetricsByComponentId(Integer componentId);
+}
