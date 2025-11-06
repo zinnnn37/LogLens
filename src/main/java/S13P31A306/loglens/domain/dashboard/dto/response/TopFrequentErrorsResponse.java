3ffe564ed257a2b63d3b12e73df46393f6eb2620
김민진 <zinnnn37@gmail.com>
2025-11-06 17:26:44 +0900

    fix: 뭔~가 Period 분리하니까 잘 안돼서 보류

--- src/main/java/S13P31A306/loglens/domain/dashboard/dto/response/TopFrequentErrorsResponse.java
+++ src/main/java/S13P31A306/loglens/domain/dashboard/dto/response/TopFrequentErrorsResponse.java
@@ -3,6 +3,7 @@ package S13P31A306.loglens.domain.dashboard.dto.response;
 import S13P31A306.loglens.global.annotation.Sensitive;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Schema(description = "자주 발생하는 에러 top N 응답")
@@ -20,6 +21,15 @@ public record TopFrequentErrorsResponse(
         @Schema(description = "에러 통계 요약")
         ErrorSummary summary
 ) {
+
+    public record Period(
+            @Schema(description = "조회 시작 시간", example = "2025-10-10T00:00:00Z")
+            LocalDateTime startTime,
+
+            @Schema(description = "조회 종료 시간", example = "2025-10-17T23:59:59Z")
+            LocalDateTime endTime
+    ) {
+    }
 
     public record ErrorInfo(
 
