58634426807c88ae3754d25ec70838388aa1a9b0
김건학 <kkh10002@naver.com>
2025-11-06 10:48:04 +0900

    feat: 프로젝트 연결 상태 조회 성공 코드 추가

--- src/main/java/S13P31A306/loglens/domain/project/constants/ProjectSuccessCode.java
+++ src/main/java/S13P31A306/loglens/domain/project/constants/ProjectSuccessCode.java
@@ -13,6 +13,7 @@ public enum ProjectSuccessCode implements SuccessCode {
     PROJECT_RETRIEVED("PJ200-2", "프로젝트 상세 정보를 성공적으로 조회했습니다.", HttpStatus.OK.value()),
     PROJECT_DELETED("PJ200-3", "프로젝트를 성공적으로 삭제했습니다.", HttpStatus.OK.value()),
     MEMBER_DELETED("PJ200-4", "멤버를 성공적으로 삭제했습니다.", HttpStatus.OK.value()),
+    PROJECT_CONNECTION_STATUS_RETRIEVED("PJ200-5", "프로젝트가 정상적으로 연결되었습니다.", HttpStatus.OK.value()),
 
     // 201 CREATED
     PROJECT_CREATED("PJ201-1", "프로젝트가 성공적으로 생성되었습니다.", HttpStatus.CREATED.value()),
