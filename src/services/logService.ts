ab85a6eefb2151ff909db0805899ee0184fdaa79
LeeJongHyun <a24358232@gmail.com>
2025-11-06 13:15:15 +0900

    feat : TraceId 값 유무에 따른 함수 호출 타입 추가

--- src/services/logService.ts
+++ src/services/logService.ts
@@ -1,16 +1,22 @@
+// src/services/logService.ts
+
 import { apiClient } from './apiClient';
 import { API_PATH } from '@/constants/api-path';
-import type { LogSearchParams, LogSearchResponse } from '@/types/log';
+import type {
+  LogSearchParams,
+  LogSearchResponse,
+  TraceIdSearchResponse,
+} from '@/types/log';
 
 /**
  * 로그 검색 API 호출
  */
 export const searchLogs = async (
   params: LogSearchParams,
-): Promise<LogSearchResponse> => {
-  const response = await apiClient.get<LogSearchResponse>(
-    API_PATH.LOGS_SEARCH,
-    params,
-  );
+): Promise<LogSearchResponse | TraceIdSearchResponse> => {
+  const response = await apiClient.get<
+    LogSearchResponse | TraceIdSearchResponse
+  >(API_PATH.LOGS_SEARCH, params);
+
   return response;
 };
