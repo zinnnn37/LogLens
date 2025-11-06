96d903768cad65e4d1e5ff15c194edb4429e6982
김민진 <zinnnn37@gmail.com>
2025-11-06 16:40:17 +0900

    refactor: projectUuid 민감 정보 어노테이션 적용

--- src/main/java/S13P31A306/loglens/domain/project/dto/response/ProjectListResponse.java
+++ src/main/java/S13P31A306/loglens/domain/project/dto/response/ProjectListResponse.java
@@ -1,5 +1,6 @@
 package S13P31A306.loglens.domain.project.dto.response;
 
+import S13P31A306.loglens.global.annotation.Sensitive;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
@@ -32,6 +33,7 @@ public record ProjectListResponse(
             @Schema(description = "프로젝트 설명", example = "로그 수집 및 분석 프로젝트")
             String description,
 
+            @Sensitive
             @Schema(description = "프로젝트 UUID", example = "pk_1a2b3c4d5e6f")
             String projectUuid,
 
