1924514bcbffc954ee72ed733cb45002ccce29a1
김건학 <kkh10002@naver.com>
2025-11-06 11:09:17 +0900

    feat: 프로젝트 존재 여부 확인 기능 구현

--- src/main/java/S13P31A306/loglens/domain/log/repository/LogRepository.java
+++ src/main/java/S13P31A306/loglens/domain/log/repository/LogRepository.java
@@ -9,4 +9,12 @@ public interface LogRepository {
     LogSearchResult findWithCursor(String projectUuid, LogSearchRequest request);
 
     TraceLogSearchResult findByTraceId(String projectUuid, LogSearchRequest request);
+
+    /**
+     * 프로젝트 UUID로 로그가 존재하는지 확인
+     *
+     * @param projectUuid 프로젝트 UUID
+     * @return true: 로그 존재, false: 로그 없음
+     */
+    boolean existsByProjectUuid(String projectUuid);
 }
