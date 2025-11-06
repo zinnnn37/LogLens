d210f3ad1c6794c194c488d95f73f122602b2bba
김건학 <kkh10002@naver.com>
2025-11-06 10:49:05 +0900

    feat: 프로젝트 연결 확인 기능 추가

--- src/main/java/S13P31A306/loglens/domain/project/service/ProjectService.java
+++ src/main/java/S13P31A306/loglens/domain/project/service/ProjectService.java
@@ -4,6 +4,7 @@ import S13P31A306.loglens.domain.project.constants.ProjectOrderParam;
 import S13P31A306.loglens.domain.project.constants.ProjectSortParam;
 import S13P31A306.loglens.domain.project.dto.request.ProjectCreateRequest;
 import S13P31A306.loglens.domain.project.dto.request.ProjectMemberInviteRequest;
+import S13P31A306.loglens.domain.project.dto.response.ProjectConnectionResponse;
 import S13P31A306.loglens.domain.project.dto.response.ProjectCreateResponse;
 import S13P31A306.loglens.domain.project.dto.response.ProjectDetailResponse;
 import S13P31A306.loglens.domain.project.dto.response.ProjectListResponse;
@@ -41,6 +42,7 @@ public interface ProjectService {
      * @return ProjectListResponse 프로젝트 리스트
      */
     ProjectListResponse getProjects(int page, int size, ProjectSortParam sort, ProjectOrderParam order);
+
     /**
      * 프로젝트 상세 조회
      *
@@ -66,4 +68,11 @@ public interface ProjectService {
      */
     void deleteMember(String projectUuid, int memberId);
 
+    /**
+     * 프로젝트 연결 상태 확인
+     *
+     * @param projectUuid 프로젝트 UUID
+     * @return ProjectConnectionResponse 프로젝트 연결 상태
+     */
+    ProjectConnectionResponse checkProjectConnection(String projectUuid);
 }
