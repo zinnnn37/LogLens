a1a2e64ae236b4566dc45a75d648db861e8d551a
김민진 <zinnnn37@gmail.com>
2025-11-13 14:48:29 +0900

    chore: .

--- test-static/lib/loglens.d.ts
+++
@@ -1,9 +0,0 @@
-export { Log } from './decorators/logs';
-export { LogFormatter } from './utils/logFormatter';
-export { withLogLens } from './wrappers/trace';
-export { LightZone } from './core/lightZone';
-export { LogCollector } from './core/logCollector';
-export { useLogLens } from './react/useLogLens';
-export { loglens } from './core/logger';
-export { useLogger } from './react/useLogger';
-export { type LogEntry } from './core/types';
