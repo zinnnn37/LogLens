28c331cc8478d815835e41b5c758905d5076746a
김민진 <zinnnn37@gmail.com>
2025-11-14 19:43:59 +0900

    hotfix: import 추가

--- src/main/java/S13P31A306/loglens/domain/dashboard/controller/impl/DashboardController.java
+++ src/main/java/S13P31A306/loglens/domain/dashboard/controller/impl/DashboardController.java
@@ -26,11 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
