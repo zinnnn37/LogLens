83b972dee3e4fad9fa3d358c8008ee6b1bdc6bd0
DESKTOP-RBNO6LS\SSAFY <01040307z@gmail.com>
2025-11-14 11:58:55 +0900

    feat(statistics): 통계 API 및 컨트롤러 추가

---
+++ src/main/java/S13P31A306/loglens/domain/statistics/controller/impl/StatisticsController.java
@@ -0,0 +1,47 @@
+package S13P31A306.loglens.domain.statistics.controller.impl;
+
+import S13P31A306.loglens.domain.statistics.constants.StatisticsSuccessCode;
+import S13P31A306.loglens.domain.statistics.controller.StatisticsApi;
+import S13P31A306.loglens.domain.statistics.dto.response.LogTrendResponse;
+import S13P31A306.loglens.domain.statistics.service.LogTrendService;
+import S13P31A306.loglens.global.annotation.ValidUuid;
+import S13P31A306.loglens.global.dto.response.ApiResponseFactory;
+import S13P31A306.loglens.global.dto.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 통계 API 컨트롤러
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/statistics")
+@RequiredArgsConstructor
+@Validated
+public class StatisticsController implements StatisticsApi {
+
+    private static final String LOG_PREFIX = "[StatisticsController]";
+
+    private final LogTrendService logTrendService;
+
+    @Override
+    @GetMapping("/log-trend")
+    public ResponseEntity<? extends BaseResponse> getLogTrend(
+            @ValidUuid @RequestParam String projectUuid
+    ) {
+        log.info("{} 로그 추이 API 호출: projectUuid={}", LOG_PREFIX, projectUuid);
+
+        LogTrendResponse response = logTrendService.getLogTrend(projectUuid);
+
+        return ApiResponseFactory.success(
+                StatisticsSuccessCode.LOG_TREND_RETRIEVED,
+                response
+        );
+    }
+}
